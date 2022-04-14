affa1b8ccbbb2031137acccc6e5ebee3c0339403
Rob Ferguson <robert.j.ferguson@nasa.gov>
2022-04-13 22:35:51 -0500

    add additional test and example

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -173,3 +173,54 @@ spec:
 		t.Errorf("s.Find() = %q, want %q", got, want)
 	}
 }
+
+func TestFindPartialMatch(t *testing.T) {
+	input := bytes.NewReader([]byte(`apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginxDeployment
+  labels:
+  app: nginx
+spec:
+  replicas: 3
+  selector:
+  matchLabels:
+    app: nginx
+  template:
+  metadata:
+    labels:
+    app: nginx
+  spec:
+    containers:
+    - name: nginx
+    image: nginx:1.7.9
+    ports:
+    - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx
+spec:
+  selector:
+  app: nginx
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
+`))
+
+	var output bytes.Buffer
+
+	Find(
+		&kio.ByteReader{Reader: input},
+		BuildFilters("nginx$", ""),
+		BuildOutputs(true, &output))
+
+	want := "Service/nginx\n"
+	got := output.String()
+
+	if got != want {
+		t.Errorf("s.Find() = %q, want %q", got, want)
+	}
+}
